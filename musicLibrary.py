77bb6a45ad8250d650f1a4179ab214374b1f29f8
arpitamalpe03 <arpitamalpe7@gmail.com>
2025-12-12 12:56:31 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "stealth" : "https://youtu.be/U47Tr9BB_wE?si=Qu3hMf64jSrzaxnH",
+    "tiranga" : "https://youtu.be/l71aOtTJ1gE?si=GU0D_rofQPobm30D",
+    "skyfall" : "https://youtu.be/sZrTJesvJeo?si=ZkrPyuxWW9o3SmBN",
+    "maa" : "https://youtu.be/pOK08cRwE6c?si=Spo_vIWKKWOUcQYC"
+}
